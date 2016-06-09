69e530213c2772ed504332996ab3e0407f6f7128
Damian Tylczyński <d.tylczynski@foodpanda.com>
2016-06-09 09:58:14 +0000

    Message parser missing messages

--- services/message-parser.ts
+++ services/message-parser.ts
@@ -1,7 +1,11 @@
 import {
     MessageParser,
     VendorOfflineOpenMessage,
-    VendorOfflineOpenMessageProcessor
+    VendorOfflineOpenMessageProcessor,
+    VendorOnlineOpenMessage,
+    VendorOnlineOpenMessageProcessor,
+    VendorOnlineClosedMessage,
+    VendorOnlineClosedMessageProcessor
 } from "fp-message-sdk/lib/message-sdk";
 
 let defaultPlatform = 'foodpanda';
@@ -10,6 +14,16 @@ let messageParser = new MessageParser(defaultPlatform);
 messageParser.registerMessageProcessor(
     VendorOfflineOpenMessage.MESSAGE_TYPE,
     new VendorOfflineOpenMessageProcessor()
+);
+
+messageParser.registerMessageProcessor(
+    VendorOnlineOpenMessage.MESSAGE_TYPE,
+    new VendorOnlineOpenMessageProcessor()
+);
+
+messageParser.registerMessageProcessor(
+    VendorOnlineClosedMessage.MESSAGE_TYPE,
+    new VendorOnlineClosedMessageProcessor()
 );
 
 export default messageParser;
